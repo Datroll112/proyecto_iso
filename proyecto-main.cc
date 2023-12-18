f7a636c55b178c8cbc199a6bb5f2867c942b9fc2
Vitto04 <vrsanchez2004@gmail.com>
2023-12-18 16:47:12 +0100

    creo el archivo main

---
+++ proyecto-main.cc
@@ -0,0 +1,16 @@
+// proyecto-main.cc
+// main con el menu y todas las opciones que se le brinda al usuario
+// en el caso de nuestro proyecto, el menu sera un programa sencillo hecho en c++ que se ejecuta en la terminal
+
+#include <iostream>
+
+    int main(){
+
+        int opcion ;
+     
+
+
+
+
+
+    }
